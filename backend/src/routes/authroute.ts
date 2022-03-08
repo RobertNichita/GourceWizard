cc96e2168740252792aad6273aa89a4ec33a5485
Robert Nichita <robert.nickykitta@gmail.com>
2022-03-07 23:41:00 -0500

    more progress

--- backend/src/routes/authroute.ts
+++ backend/src/routes/authroute.ts
@@ -1,16 +1,23 @@
-import { body } from 'express-validator';
-import express, { Router } from 'express';
+import express, {Response, Request, Router, NextFunction} from 'express';
 
 const router: Router = express.Router();
 
 const isAuthenticated = () => {};
 
-router.post('/signup/', (req, res, next) => {});
+router.post(
+  '/signup/',
+  (req: Request, res: Response, next: NextFunction) => {}
+);
 
-router.post('/signin/', (req, res, next) => {});
+router.post(
+  '/signin/',
+  (req: Request, res: Response, next: NextFunction) => {}
+);
 
-router.get('/signout/', isAuthenticated, (req, res, next) => {});
+router.get(
+  '/signout/',
+  isAuthenticated,
+  (req: Request, res: Response, next: NextFunction) => {}
+);
 
-router.get('/callback/', (req, res, next) => {});
-
-export { router, isAuthenticated };
+export {router, isAuthenticated};
