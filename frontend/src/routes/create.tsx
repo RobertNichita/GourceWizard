5043f13022433cddbf6fbc696dd62d2cd852ddc4
darmadillo <dar.liu@mail.utoronto.ca>
2022-03-18 19:15:39 -0400

    initial data form

--- frontend/src/routes/create.tsx
+++ frontend/src/routes/create.tsx
@@ -1,5 +1,4 @@
 import {Button} from '../components/Button';
-import {Videos} from '../components/video/Videos';
 import {useNavigate} from 'react-router-dom';
 
 export default function library() {
@@ -7,18 +6,98 @@ export default function library() {
   return (
     <div>
       <h1 className="margin">🧙 Gource Wizard ✨</h1>
-      <div className="relative">
-        <Button
-          className="fixed bottom-5 right-5 p-5 text-xl"
-          title="Finish Brewing 🧪"
-          onClick={() => {
-            console.log('brewbrewbew');
-          }}
-        ></Button>
-      </div>
-
       <div className="flex h-screen items-center justify-center flex-col mx-10">
-        <p className="margin text-center text-5xl">Potion Room.</p>
+        <div className="flex items-start justify-center flex-col m-10 p-10 rounded-lg shadow-lg">
+          <p className="margin text-5xl">Potion Room.</p>
+
+          {/* Form Design from: https://v1.tailwindcss.com/components/forms# */}
+          <form className="w-full max-w-lg">
+            <div className="flex flex-wrap -mx-3">
+              <div className="w-full px-3">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-url"
+                >
+                  Repository URL
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-url"
+                  type="text"
+                  placeholder="https://github.com/acaudwell/Gource"
+                ></input>
+              </div>
+            </div>
+            <div className="flex flex-wrap -mx-3 mb-2">
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-title"
+                >
+                  Title
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-city"
+                  type="title"
+                  placeholder="Video Title"
+                ></input>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-state"
+                >
+                  Visibility
+                </label>
+                <div className="relative">
+                  <select
+                    className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-state"
+                  >
+                    <option>Public</option>
+                    <option>Private</option>
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg
+                      className="fill-current h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-wrap -mx-3">
+              <div className="w-full px-3">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-url"
+                >
+                  Description
+                </label>
+                <textarea
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-url"
+                  placeholder="Describe the repository a little."
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="flex items-end justify-end">
+              <Button
+                className="-mx-0"
+                title="Next Step 🧪"
+                onClick={() => {
+                  console.log('brewbrewbew');
+                }}
+              ></Button>
+            </div>
+          </form>
+        </div>
 
         <Button
           title="Wizards Tower 🧙"
