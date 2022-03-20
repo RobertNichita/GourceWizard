c16fc650cbfa19f9da432f5a27db07b9f2033e0e
Robert Nichita <robert.nickykitta@gmail.com>
2022-03-20 13:15:25 -0400

    fix user resolver progress

--- backend/src/resolvers/user_resolver.ts
+++ backend/src/resolvers/user_resolver.ts
@@ -3,6 +3,6 @@ import {ExpressContext} from 'apollo-server-express';
 export const resolvers = {
   Query: {
     me: (parent: any, args: any, context: ExpressContext, info: any) =>
-      context.req.user,
+      context.req.passport!.user.user,
   },
 };
