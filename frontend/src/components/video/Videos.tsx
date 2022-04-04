8b6a93645b1301bec33f446ab438deccc9e8bdc7
darmadillo <dar.liu@mail.utoronto.ca>
2022-04-04 18:54:17 -0400

    text alignment issue with empty library fixed

--- frontend/src/components/video/Videos.tsx
+++ frontend/src/components/video/Videos.tsx
@@ -2,13 +2,21 @@ import {Video} from './Video';
 
 export function Videos(props) {
   const {items} = props;
-  return (
-    <div className="grid grid-cols-3">
-      {items.length === 0 ? (
-        <p className="text-center">No videos added yet, try adding some.</p>
-      ) : (
-        items.map((item, idx) => <Video key={`item-idx-${idx}`} data={item} />)
-      )}
-    </div>
-  );
+  if (items.length === 0) {
+    return (
+      <div>
+        <p className="text-center w-full">
+          No videos added yet, try adding some.
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="grid grid-cols-3">
+        {items.map((item, idx) => (
+          <Video key={`item-idx-${idx}`} data={item} />
+        ))}
+      </div>
+    );
+  }
 }
