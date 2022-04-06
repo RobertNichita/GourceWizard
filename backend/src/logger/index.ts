605fe002254061becd3305e3a0b128d39fd9db44
Ryan Sue <Raieen@users.noreply.github.com>
2022-04-06 15:24:22 -0400

    Change logger transports

--- backend/src/logger/index.ts
+++ backend/src/logger/index.ts
@@ -15,20 +15,11 @@ const logger = winston.createLogger({
     //
     new winston.transports.File({filename: 'error.log', level: 'error'}),
     new winston.transports.File({filename: 'combined.log'}),
-  ],
-});
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
     new winston.transports.Console({
       format: winston.format.simple(),
     })
-  );
-}
+  ],
+});
 
 const log = (
   message: string,
