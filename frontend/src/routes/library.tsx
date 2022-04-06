5dcfdecc81ac534ea4623c485654687bfac596b3
darmadillo <dar.liu@mail.utoronto.ca>
2022-04-06 11:38:49 -0400

    better loading animation for library

--- frontend/src/routes/library.tsx
+++ frontend/src/routes/library.tsx
@@ -4,6 +4,7 @@ import {AppBanner} from '../components/navigation/AppBanner';
 import {IVideoService, VideoService} from '../services/video_service';
 import {useNavigate} from 'react-router-dom';
 import {useEffect, useState} from 'react';
+import DotLoader from 'react-spinners/DotLoader';
 
 export default function library() {
   const navigate = useNavigate();
@@ -14,7 +15,7 @@ export default function library() {
    * Pagination
    */
   const [page, setPage] = useState(0);
-
+  const [busy, setBusy] = useState(true);
   const videoService: IVideoService = new VideoService();
 
   const deleteVideo = itemIdx => {
@@ -29,6 +30,7 @@ export default function library() {
   useEffect(() => {
     console.log('Loading library videos');
     videoService.getVideos(page).then(videos => {
+      setBusy(false);
       setVideos(videos);
     });
   }, []);
@@ -61,34 +63,44 @@ export default function library() {
         ></Button>
       </div>
 
-      <div className="flex items-center justify-center flex-col mx-10">
+      <div className="flex items-center justify-center flex-col mx-10 h-full">
         <p className="margin text-center text-5xl">Library.</p>
 
-        <Videos
-          className="text-center bg-g"
-          items={videos}
-          deleteVideo={deleteVideo}
-        ></Videos>
+        {busy ? (
+          <div className="relative h-screen">
+            <div className="absolute top-1/3 right-1/2">
+              <DotLoader size={45} />
+            </div>
+          </div>
+        ) : (
+          <div>
+            <Videos
+              className="text-center bg-g"
+              items={videos}
+              deleteVideo={deleteVideo}
+            ></Videos>
 
-        <div className="flex items-center">
-          <Button
-            title="Previous"
-            className="px-4 py-2 font-bold text-gray-700 bg-gray-200 rounded-md hover:bg-gray-400 hover:text-white"
-            onClick={() => {
-              navigate('/library');
-            }}
-          ></Button>
-          <p className="px-10 py-2 text-gray-700 bg-gray-200 rounded-md font-mono">
-            {page + 1}
-          </p>
-          <Button
-            title="Next"
-            className="px-4 py-2 font-bold text-gray-700 bg-gray-200 rounded-md hover:bg-gray-400 hover:text-white"
-            onClick={() => {
-              navigate('/library');
-            }}
-          ></Button>
-        </div>
+            <div className="flex items-center">
+              <Button
+                title="Previous"
+                className="px-4 py-2 font-bold text-gray-700 bg-gray-200 rounded-md hover:bg-gray-400 hover:text-white"
+                onClick={() => {
+                  navigate('/library');
+                }}
+              ></Button>
+              <p className="px-10 py-2 text-gray-700 bg-gray-200 rounded-md font-mono">
+                {page + 1}
+              </p>
+              <Button
+                title="Next"
+                className="px-4 py-2 font-bold text-gray-700 bg-gray-200 rounded-md hover:bg-gray-400 hover:text-white"
+                onClick={() => {
+                  navigate('/library');
+                }}
+              ></Button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
